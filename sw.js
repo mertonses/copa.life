2fde709b73efea662ed444d64e349de810fb1b0d
mertonses <mrtonses1@gmail.com>
2026-07-13 19:14:04 +0300

    fix: restore dark theme contrast

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* copa.life Service Worker - v20260713-theme-sound2 */
-const CACHE = "copa-v20260713-theme-sound2";
+/* copa.life Service Worker - v20260713-theme-sound3 */
+const CACHE = "copa-v20260713-theme-sound3";
 const PRECACHE = [
   "/",
   "/index.html",
